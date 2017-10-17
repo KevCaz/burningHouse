671f2ab32226897c92737dfd22f2a8595a6f714d
Kevin Cazelles <kcazelle@uoguelph.ca>
2017-10-17 16:11:03 -0400

    add first data monthxxx and papers_bd

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' Monthly record of publicatoin in Canada for cc and bd
+#'
+#' Monthly record of publicatoin in Canada for cc and bd
+#'
+#' @docType data
+#' @keywords datasets
+#' @author tobeadded
+#' @name monthcan
+#' @usage data(monthcan)
+#' @format A data frame of 324 rows and 3 columns
+"monthcan"
+
+#' Monthly record of publicatoin in USA for cc and bd
+#'
+#' Monthly record of publicatoin in USA for cc and bd
+#'
+#' @docType data
+#' @keywords datasets
+#' @author tobeadded
+#' @name monthgbr
+#' @usage data(monthgbr)
+#' @format A data frame of 324 rows and 3 columns
+"monthgbr"
+
+
+#' Monthly record of publicatoin in usa for cc and bd
+#'
+#' Monthly record of publicatoin in usa for cc and bd
+#'
+#' @docType data
+#' @keywords datasets
+#' @author tobeadded
+#' @name monthusa
+#' @usage data(monthusa)
+#' @format A data frame of 324 rows and 3 columns
+"monthusa"
+
+
+#' Papers biodiversity
+#'
+#' Papers biodiversity
+#'
+#' @docType data
+#' @keywords datasets
+#' @author tobeadded
+#' @name papers_bd
+#' @usage data(papers_bd)
+#' @format A list of 20 elements
+"papers_bd"
---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+#' burningHouse
+#'
+#' @name burningHouse
+#' @docType package
+#' @description Code implementation for Legagneux et al. 2017.
+NULL
